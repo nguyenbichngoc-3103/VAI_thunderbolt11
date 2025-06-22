49e9008c447e181cb8f8faefdf6ab928a2ece63b
anhkhoa1101 <claudetrading00@gmail.com>
2025-06-22 23:12:23 +0700

    Finish-v7

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Cấu hình tùy chỉnh của bạn ở đây
-  root: './zalo-mini-app',  // Thư mục gốc chứa tệp index.html
+  root: './dist',  // Thư mục gốc chứa tệp index.html
   build: {
     outDir: 'dist',  // Thư mục xuất ra sau khi build
   },
